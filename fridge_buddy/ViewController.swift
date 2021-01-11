1bcaf69a8119c139da3662cc92da1d733590d82b
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2021-01-11 10:34:31 +0800

    Initial Commit

---
+++ fridge_buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fridge_buddy
+//
+//  Created by Gustavo rodrigues on 2021/1/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
