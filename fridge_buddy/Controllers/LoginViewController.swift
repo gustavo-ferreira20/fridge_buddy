0b1137332a6af4f16d606b41391e5d5c2ff3ed02
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2021-01-12 13:07:33 +0800

    Add Login Screen

---
+++ fridge_buddy/Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  fridge_buddy
+//
+//  Created by Gustavo rodrigues on 2021/1/12.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
