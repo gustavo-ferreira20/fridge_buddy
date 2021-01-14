8c467e7a05092f031af348e9e367139e53e386d3
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2021-01-14 16:14:28 +0800

    Fix Navbar background landscape mode bug

--- fridge_buddy/Controllers/LoginViewController.swift
+++ fridge_buddy/Controllers/LoginViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class LoginViewController: UIViewController {
     let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 39, height: 39))
     let image = UIImage(named: "logo-text-1.png")
-    let background = UIImage(named: "Mango.png")
+   // let background = UIImage(named: "Mango.png")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,42 @@ class LoginViewController: UIViewController {
         navigationItem.titleView = imageView
         
         // Adding a background image to the Nav bar
-        navigationController?.navigationBar.setBackgroundImage(background, for: .default)
+      //navigationController?.addBackground(imageName: "Mango.png")
+       addBackgroundNavBar(imageName: "Mango.png")
         
     }
+    
+    
+    //adding background image to nav bar
+    func addBackgroundNavBar(imageName: String)  {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: imageName)?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0), resizingMode: .stretch), for: .default)
+    }
+    
 }
+
+
+//extension UINavigationController {
+//    func addBackground(imageName: String = "YOUR DEFAULT IMAGE NAME") {
+//        // setup the UIImageView
+//        let bgImage = UIImage(named: imageName)
+//
+//
+//        let logoImageView = UIImageView(image: bgImage)
+//        logoImageView.contentMode = .scaleAspectFill
+//        logoImageView.clipsToBounds = true
+//        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        navigationBar.addSubview(logoImageView)
+//        navigationBar.sendSubviewToBack(logoImageView)
+//        navigationBar.backgroundImage(for: .default)
+//       // navigationBar.insertSubview(logoImageView, belowSubview: navigationBar)
+//
+//          NSLayoutConstraint.activate([
+//            logoImageView.leftAnchor.constraint(equalTo: navigationBar.leftAnchor),
+//            logoImageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor),
+//            logoImageView.topAnchor.constraint(equalTo: navigationBar.topAnchor),
+//            logoImageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor)
+//          ])
+//
+//    }
+//}
